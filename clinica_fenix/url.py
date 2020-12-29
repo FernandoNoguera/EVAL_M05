b412f255beb6f969aa843c6e94bb2366d0ddbd1e
Fernando Noguera <fnoguerav25@gmail.com>
2020-12-29 20:54:35 -0300

    cambios en el proyecto en general

--- clinica_fenix/url.py
+++ clinica_fenix/url.py
@@ -5,6 +5,6 @@ app_name = "clinica_fenix"
 
 urlpatterns = [
     path("", views.inicio),
-    path("login/", views.login)
-       
+    path("login/", views.login, name = "login" ),
+    path("pagina_privada/", views.private_page, name ="portal_privado" ),
 ]
