5e14fbb9c3a2325704804fb5bf53720a34c6d3a6
Fernando Noguera <fnoguerav25@gmail.com>
2020-12-30 19:28:23 -0300

    agregando nuevos clientes

--- clinica_fenix/forms.py
+++ clinica_fenix/forms.py
@@ -3,6 +3,12 @@ from django import forms
 
 class IngresoUsuario(forms.Form):
     usuario = forms.CharField()
-    #contraseña = forms.CharField()
-    #contraseña2 = forms.PasswordInput()
+    clave = forms.CharField(widget=forms.PasswordInput)
+
+class new_user(forms.Form):
+    nombre = forms.CharField()
+    apellido = forms.CharField()
+    edad = forms.CharField(widget=forms.NumberInput)
+    rut = forms.CharField()
+ 
  
--- clinica_fenix/views.py
+++ clinica_fenix/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect 
 from django.conf import settings
-from .forms import IngresoUsuario
+from .forms import IngresoUsuario,new_user
 import json
 
 def inicio(request):  
@@ -17,12 +17,30 @@ def login(request):
         usuario['usuario'].append(form_data)
         with open(str(settings.BASE_DIR)+filename, 'w') as file:
             json.dump(usuario, file)
-        return redirect('clinica_fenix:portal_privado')
-        
-
-                
+        return redirect('clinica_fenix:portal_privado')               
     return render(request, 'clinica_py/registro.html',context)
 
 def private_page(request):
     return render(request, 'clinica_py/PagePrivate.html')
+
+def nuevo_usuario(request):
+    formulario = new_user(request.POST or None)
+    context = {'form':formulario}
+    if formulario.is_valid():
+        form_data = formulario.cleaned_data
+        filename= "/clinica_py/static/clinica_py/data/clientes.json"
+        with open(str(settings.BASE_DIR)+filename, 'r') as file:
+            usuario=json.load(file)
+        usuario['usuario'].append(form_data)
+        with open(str(settings.BASE_DIR)+filename, 'w') as file:
+            json.dump(usuario, file)
+        return redirect('clinica_fenix:lista_usuario')               
+    return render(request, 'clinica_py/new_user.html', context)
+
+def usuarios_registrados(request):
+    filename= "/clinica_py/static/clinica_py/data/clientes.json"
+    with open(str(settings.BASE_DIR)+filename, 'r') as file:
+        clientes=json.load(file)
+    return render(request, 'clinica_py/lista_usuario.html', context=clientes)
+    
 
