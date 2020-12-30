0b5a1c2beec743d6f1431ab10f93fc767846b20d
Fernando Noguera <fnoguerav25@gmail.com>
2020-12-30 09:18:34 -0300

    se agrega form para registro de usuario

--- clinica_fenix/views.py
+++ clinica_fenix/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.conf import settings
+from .forms import IngresoUsuario
+import json
 
 def inicio(request):  
     return render(request, 'clinica_py/index.html')
 
 def login(request):
-    return render(request, 'clinica_py/registro.html')
+    formulario = IngresoUsuario(request.POST or None)
+    context = {'form':formulario}
+    if formulario.is_valid():
+        form_data = formulario.cleaned_data
+        filename= "/clinica_py/static/clinica_py/data/usuario.json"
+        with open(str(settings.BASE_DIR)+filename, 'r') as file:
+            usuario=json.load(file)
+        usuario['usuario'].append(form_data)
+        with open(str(settings.BASE_DIR)+filename, 'w') as file:
+            json.dump(usuario, file)
+        return redirect('clinica_fenix:portal_privado')
+        
+
+                
+    return render(request, 'clinica_py/registro.html',context)
 
 def private_page(request):
-    nombre = 
     return render(request, 'clinica_py/PagePrivate.html')
+
