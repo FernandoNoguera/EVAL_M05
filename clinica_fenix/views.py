e9ff3450b8d9176d0fff5ae00b2265396543edeb
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-12-30 21:05:26 -0300

    validacion

--- clinica_fenix/views.py
+++ clinica_fenix/views.py
@@ -14,10 +14,14 @@ def login(request):
         filename= "/clinica_py/static/clinica_py/data/usuario.json"
         with open(str(settings.BASE_DIR)+filename, 'r') as file:
             usuario=json.load(file)
-        usuario['usuario'].append(form_data)
-        with open(str(settings.BASE_DIR)+filename, 'w') as file:
-            json.dump(usuario, file)
-        return redirect('clinica_fenix:portal_privado')               
+        if (form_data['usuario'] == usuario['usuario']) and (form_data['clave'] == usuario['clave']):
+            return redirect('clinica_fenix:portal_privado')               
+        else:
+#            print("No tiene acceso")
+            return redirect('clinica_fenix:login')               
+#        usuario['usuario'].append(form_data)
+#        with open(str(settings.BASE_DIR)+filename, 'w') as file:
+#            json.dump(usuario, file)
     return render(request, 'clinica_py/registro.html',context)
 
 def private_page(request):
